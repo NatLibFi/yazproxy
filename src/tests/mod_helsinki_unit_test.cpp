fb76fcae76324279f32a962ddc6b4158079d542b
Ronja Koistinen <ronja.koistinen@helsinki.fi>
2025-11-03 18:05:42 +0200

    Add Google Test support

---
+++ src/tests/mod_helsinki_unit_test.cpp
@@ -0,0 +1,32 @@
+/* This file is part of YAZ proxy
+   Copyright (C) 1998-2011 Index Data
+
+YAZ proxy is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2, or (at your option) any later
+version.
+
+YAZ proxy is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include <stdlib.h>
+
+
+int main(int argc, char **argv)
+{
+    exit(0);
+}
+/*
+ * Local variables:
+ * c-basic-offset: 4
+ * c-file-style: "Stroustrup"
+ * indent-tabs-mode: nil
+ * End:
+ * vim: shiftwidth=4 tabstop=8 expandtab
+ */
